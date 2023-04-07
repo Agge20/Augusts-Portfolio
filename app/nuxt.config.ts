037bd0fe186e46472f84f6483699d76e9b521e89
august <augustraro112@hotmail.com>
2023-04-08 01:24:46 +0200

    Installed General Sans font.

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: ["@nuxtjs/tailwindcss"],
+    tailwindcss: {
+        configPath: "~/config/tailwind.js",
+    },
+    css: [
+        // Load a CSS file from a local module
+        "@/assets/css/main.css",
+        "@/assets/css/fonts.css",
+    ],
 });
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,1 +1,6 @@
-<template></template>
+<template>
+    <div class="bg-ap-white-100">
+        <h1 class="font-gs-">Index</h1>
+        <p>My first Nuxt.js app</p>
+    </div>
+</template>
