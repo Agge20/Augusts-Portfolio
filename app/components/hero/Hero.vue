f94f47b663a6685005321207a2b4f0a258c80095
august <augustraro112@hotmail.com>
2023-04-13 14:32:39 +0200

    Installed and setup Motion One animation library to be used in Hero component.

---
+++ app/components/hero/Hero.vue
@@ -0,0 +1,37 @@
+<template>
+    <particles id="tsparticles" :particlesInit="particlesInit" :options="particlesTSConfig" />
+    <h1 id="scene-1" class="bg-ap-white-100 opacity-0">Hello</h1>
+    <h2 id="scene-2">My name is <span>August</span></h2>
+    <h3 id="scene-3">I build stuff with:</h3>
+</template>
+
+<script setup lang="ts">
+    // Import particleTS config
+    import { particlesTSConfig } from "@/config/particlesTS";
+    import { loadFull } from "tsparticles";
+
+    // Load motion one
+    import { animate, timeline } from "motion";
+
+    // Import types
+    import { TimelineItem } from "@/types/MotionOneTypes";
+    import { TimelineDefinition, TimelineSegment } from "@motionone/dom/types/timeline/types";
+
+    const particlesInit = async (engine: any) => {
+        await loadFull(engine);
+    };
+
+    // When component is mounted
+    onMounted(() => {
+        const hello: TimelineSegment = [
+            "#scene-1",
+            {
+                opacity: 1,
+            },
+            { duration: 4 },
+        ];
+
+        // Create timeline
+        timeline([hello], { duration: 1 });
+    });
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,17 +1,6 @@
 <template>
     <main class="bg-ap-dark-100 min-h-screen">
         <navbar />
-        <Particles id="tsparticles" :particlesInit="particlesInit" :options="particlesTSConfig" />
+        <hero />
     </main>
 </template>
-
-<script setup lang="ts">
-    // Import particleTS config
-    import { particlesTSConfig } from "@/config/particlesTS";
-
-    import { loadFull } from "tsparticles";
-
-    const particlesInit = async (engine: any) => {
-        await loadFull(engine);
-    };
-</script>
