6008a13f0f0dabc754cf80b8b25afc392b7fb535
august <augustraro112@hotmail.com>
2023-05-29 13:38:07 +0200

    ChangeLightTheme component now has green gradient background instead of solid white or  black.

--- app/components/ChangeLightTheme.vue
+++ app/components/ChangeLightTheme.vue
@@ -2,20 +2,10 @@
     <div
         @click="mainStore.toggleTheme()"
         :class="mainStore.theme === 'light' ? 'bg-dark-100' : 'bg-white'"
-        class="group absolute left-4 top-4 z-30 flex h-16 w-16 cursor-pointer items-center justify-center rounded-full transition md:bottom-4 md:top-auto"
+        class="group absolute left-4 top-4 z-30 flex h-16 w-16 cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-green-100 via-green-200 to-green-300 transition md:bottom-4 md:top-auto"
     >
-        <icon
-            v-if="mainStore.theme === 'light'"
-            name="jam:moon-f"
-            size="3em"
-            class="bg-dark-100 rounded-full text-white transition group-hover:scale-110"
-        />
-        <icon
-            v-if="mainStore.theme === 'dark'"
-            name="jam:sun-f"
-            size="3em"
-            class="text-dark-100 rounded-full bg-white transition group-hover:scale-110"
-        />
+        <icon v-if="mainStore.theme === 'light'" name="jam:moon-f" size="3em" class="text-dark-200 rounded-full transition group-hover:scale-110" />
+        <icon v-if="mainStore.theme === 'dark'" name="jam:sun-f" size="3em" class="rounded-full text-white transition group-hover:scale-110" />
     </div>
 </template>
 
