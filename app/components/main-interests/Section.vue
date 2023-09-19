fcd55961bb3b5967caeca6d4fe61062b7df7a079
august <augustraro112@hotmail.com>
2023-09-19 20:56:11 +0200

    Main interests card animation is now toggleable.

--- app/components/main-interests/Section.vue
+++ app/components/main-interests/Section.vue
@@ -1,13 +1,17 @@
 <template>
     <div>
         <heading ref="myMainInterests" :size="HeadingSize.H2" :headingText="'My main'" :highlightText="'interests'" :highlightAlignment="'end'" />
-        <div>
-            <main-interests-card />
+        <div class="mt-8 flex flex-wrap gap-x-8">
+            <main-interests-card v-for="(mainInterest, index) in mainInterestsData" />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-    // Import the allowed heading sizes
+    /* Types */
     import { HeadingSize } from "@/types/HeadingTypes";
+    import type { MainInterest } from "@/types/MainInterest";
+
+    // Fetch all the skills content data
+    const { data: mainInterestsData }: { data: MainInterest[] } = await useAsyncData("home", () => queryContent("/json/main-interests").find());
 </script>
