86646720fd91089a494070cc53250272e9df4581
august <augustraro112@hotmail.com>
2023-11-15 22:36:09 +0100

    Text and heading now support light theme colors.

--- app/components/TextAndHeading.vue
+++ app/components/TextAndHeading.vue
@@ -1,10 +1,11 @@
 <template>
   <article
     class="mb-24 relative max-w-4xl p-2 drop-shadow-lg before:bg-gradient-to-r before:rounded-lg before:from-blue-100 before:via-turquoise-100 before:to-purple-100 before:w-full before:h-full before:absolute before:-left-0 before:top-0 before:animate-gradient-shine after:absolute after:bottom-0 after:left-0 after:h-full after:w-full after:bg-gradient-to-tr after:from-blue-100 after:via-purple-100 after:to-purple-200 after:animate-gradient-shine-and-scale after:blur-md">
-    <div class="bg-dark-100 p-8 z-10 relative w-full h-full rounded-lg">
+    <div :class="[theme === 'light' ? 'bg-white text-dark-200' : 'bg-dark-100 text-white']"
+      class="p-8 z-10 relative w-full h-full rounded-lg">
       <heading :size="HeadingSize.H2" :headingText="headingText" :highlightText="highlightText"
         :highlightAlignment="highlightAlignment" />
-      <div class="mt-2 text-lg" :class="mainStore.theme === 'light' ? 'text-dark-200' : 'text-white'" v-html="text"></div>
+      <div class="mt-2 text-lg" v-html="text"></div>
     </div>
   </article>
 </template>
@@ -24,7 +25,11 @@ import { HeadingSize } from "@/types/HeadingTypes";
 
 /* Store imports */
 import { useMainStore } from "~/stores/MainStore";
+import { storeToRefs } from "pinia";
 
 // Use the main store
 const mainStore = useMainStore();
+
+// Import the theme-state from the main store and destructure it into a ref
+const { theme } = storeToRefs(mainStore);
 </script>
