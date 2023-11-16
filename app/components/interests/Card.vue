6b5d87d300136d72d5a0d65350deb50f51eee2c6
august <augustraro112@hotmail.com>
2023-11-16 19:44:41 +0100

    General improvements to the main interests section.

--- app/components/interests/Card.vue
+++ app/components/interests/Card.vue
@@ -2,15 +2,15 @@
   <button aria-label="Interest" @mouseover="showText = true" @mouseleave="showText = false"
     @touchStart="showText = !showText" @touchEnd="showText = !showText"
     class="relative w-full min-h-88 p-4 bg-blue-100 rounded-xl bg-cover bg-center transition flex flex-col justify-start items-start lg:w-1/3 hover:scale-102 lg:hover:scale-105"
-    :style="`background-image: url(${imagePath})`">
+    :style="`background-image: url(${interest.imagePath})`">
     <div :class="showText ? 'opacity-50' : 'opacity-25'"
       class="bg-dark-200 absolute top-0 left-0 w-full h-full  transition-opacity"></div>
     <div :class="{ 'opacity-0': showText }" class="relative flex flex-col items-start z-10 transition-opacity">
       <interests-vignette :text="'Interest'" />
-      <interests-card-heading :text="title" />
+      <interests-card-heading :text="interest.title" />
     </div>
     <p :class="{ 'opacity-100': showText }" class="relative z-10 text-white opacity-0 transition-opacity text-left">
-      {{ text }}
+      {{ interest.text }}
     </p>
   </button>
 </template>
@@ -18,8 +18,12 @@
 <script setup lang="ts">
 import type { Interest } from "@/types/Interest";
 
+interface Props {
+  interest: Interest;
 
-defineProps<Interest>();
+}
+
+defineProps<Props>();
 
 const showText = ref<boolean>(false);
 
--- app/components/interests/Section.vue
+++ app/components/interests/Section.vue
@@ -2,8 +2,7 @@
   <article class="mb-24 max-w-4xl">
     <heading :size="HeadingSize.H2" :headingText="'My main'" :highlightText="'interests'" :highlightAlignment="'end'" />
     <div class="mt-8 flex flex-col gap-4 md:gap-8 lg:flex-row">
-      <interests-card v-for="(interest, index) in  interestsData" :key="index" :title="interest.title"
-        :imagePath="interest.imagePath" :text="interest.text" />
+      <interests-card v-for="(interest, index) in  interestsData" :key="index" :interest="interest" />
     </div>
   </article>
 </template>
@@ -13,5 +12,30 @@
 import { HeadingSize } from "@/types/HeadingTypes";
 import type { Interest } from "@/types/Interest";
 
-const { data: interestsData }: { data: Interest[] } = await useAsyncData("interests", () => queryContent("json", "interests").find());
+// The fetched data is mapped to the `Interest` type.
+const interestsData = ref<Interest[]>();
+
+/**
+* Fetches Nuxt Content JSON-objects from the "interests" directory.
+*
+* This function uses the `useAsyncData` hook to fetch data from the "interests" endpoint.
+* The data is then mapped to the `Interest` type.
+*
+* @returns A reactive data object with the fetched JSON-data.
+*/
+const { data } = await useAsyncData("interests", async () => {
+  const result = await queryContent("json", "interests").find();
+  return result.map((item: any): Interest => ({
+    title: item.title,
+    imagePath: item.imagePath,
+    text: item.text,
+  }));
+});
+
+
+watchEffect(() => {
+  // Watch for changes in the `data` object
+  interestsData.value = data.value ?? [];
+})
+
 </script>
