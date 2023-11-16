d302fbcb805f7cb325fc340181901d595a0985d8
august <augustraro112@hotmail.com>
2023-11-16 13:16:26 +0100

    First anchor tag on side now scrolls user to main-content.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,29 +1,32 @@
 <template>
+  <a href="#main" aria-label="Go to main content"></a>
   <spinner v-if="!themeHasBeenLoaded" />
-  <main v-if="themeHasBeenLoaded" :class="theme === 'light' ? 'bg-white' : 'bg-dark-100'"
+  <div v-if="themeHasBeenLoaded" :class="theme === 'light' ? 'bg-white' : 'bg-dark-100'"
     class="min-h-screen overflow-x-hidden pb-64 transition">
     <navbar />
-    <!-- Center content -->
-    <div class="mx-auto max-w-screen-2xl px-4 md:px-8 lg:px-12 xl:px-16">
-      <hero />
-      <change-light-theme />
-    </div>
-    <div class="relative z-20 mt-8 mx-auto max-w-screen-2xl px-4 md:px-8 lg:px-12 xl:px-16">
-      <!-- About me text section -->
-      <text-and-heading v-if="aboutMeTextData" ref="aboutMe" v-element-visibility="onAboutMeVisibility"
-        :headingText="aboutMeTextData.heading_text" :highlightText="aboutMeTextData.highlight_text"
-        :highlightAlignment="'start'" :text="aboutMeTextData.text"
-        :class="{ 'animate-fade-in mt-16 opacity-100 transition': aboutMeIsVisible, 'opacity-0': !aboutMeIsVisible }"
-        class="mb-24" />
-      <!-- Main interests section -->
-      <interests-section ref="myInterests" v-element-visibility="onMyInterestsVisibility"
-        :class="{ 'animate-fade-in opacity-100 transition': myInterestsIsVisible, 'opacity-0': !myInterestsIsVisible }" />
+    <main id="main">
+      <!-- Center content -->
+      <div class="mx-auto max-w-screen-2xl px-4 md:px-8 lg:px-12 xl:px-16">
+        <hero />
+        <change-light-theme />
+      </div>
+      <div class="relative z-20 mt-8 mx-auto max-w-screen-2xl px-4 md:px-8 lg:px-12 xl:px-16">
+        <!-- About me text section -->
+        <text-and-heading v-if="aboutMeTextData" ref="aboutMe" v-element-visibility="onAboutMeVisibility"
+          :headingText="aboutMeTextData.heading_text" :highlightText="aboutMeTextData.highlight_text"
+          :highlightAlignment="'start'" :text="aboutMeTextData.text"
+          :class="{ 'animate-fade-in mt-16 opacity-100 transition': aboutMeIsVisible, 'opacity-0': !aboutMeIsVisible }"
+          class="mb-24" />
+        <!-- Main interests section -->
+        <interests-section ref="myInterests" v-element-visibility="onMyInterestsVisibility"
+          :class="{ 'animate-fade-in opacity-100 transition': myInterestsIsVisible, 'opacity-0': !myInterestsIsVisible }" />
 
-      <!-- Contact section -->
-      <text-and-heading :headingText="'Want to'" :highlightText="'contact me?'" :highlightAlignment="'end'"
-        :text="'<a>Email: augustraro112@hotmail.com</a><br><p>Phone number: +0725662442</p>'" class="mb-24" />
-    </div>
-  </main>
+        <!-- Contact section -->
+        <text-and-heading :headingText="'Want to'" :highlightText="'contact me?'" :highlightAlignment="'end'"
+          :text="'<a>Email: augustraro112@hotmail.com</a><br><p>Phone number: +0725662442</p>'" class="mb-24" />
+      </div>
+    </main>
+  </div>
 </template>
 
 <script setup lang="ts">
